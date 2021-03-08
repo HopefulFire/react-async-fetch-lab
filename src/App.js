64941270c701dad9ef1ee1b19757691507f5732c
Jade Lynn Masker <jadelynn@masker.family>
2021-03-07 20:00:50 -0500

    commit tree f52e6739cb15f44c55261eafa94233abbe87ade0

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React from 'react';
+
+export default class App extends React.Component {
+
+  state = {
+    astronauts: {
+      people: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json").then((response) => {
+      return response.json()
+    }).then((astronauts) => {
+      this.setState({astronauts: astronauts})
+    })
+  }
+
+  render() {
+    const astronauts = this.state.astronauts.people.map((astronaut) => {
+      return <p>{astronaut.name}</p>
+    })
+    return (
+      <div>{astronauts}</div>
+    )
+  }
+}
